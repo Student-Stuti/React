7c39a2d693acf952497f39fbedfa239d9b889d30
Student Stuti <stutisharma8912@gmail.com>
2025-02-14 11:59:11 +0530

    added add value project

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>hello</h1>;
+  // let counter = 15;
+  let [counter, setCounter] = useState(15);
+
+  let addValue = () => {
+    counter = counter + 1;
+    //console.log(counter);
+    setCounter(counter);
+  };
+
+  let removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>Add and remove value </h1>
+      <h2>Counter value :{counter} </h2>
+
+      <button onClick={addValue}>Add Value{counter}</button>
+      <br></br>
+      <button onClick={removeValue}>Remove Value {counter} </button>
+    </>
+  );
 }
 
 export default App;
