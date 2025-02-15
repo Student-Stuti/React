1c291ef7af8e93a9a5d45a80e3c7c648414b34ff
Student Stuti <stutisharma8912@gmail.com>
2025-02-15 23:03:56 +0530

    added files in project

--- 05ReactRouter/src/App.jsx
+++ 05ReactRouter/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-   <h1 className='bg-green-600 p-4'>React Router</h1>
+  <Header/>
+  <Home/>
+  <Footer/>
    </>
   )
 }
--- 05ReactRouter/src/main.jsx
+++ 05ReactRouter/src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './components/about/About.jsx'
+import Home from './components/home/Home.jsx'
+import Layout from './components/Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout/>,
+    children:[
+      {
+        path : "",
+        element : <Home/>
+      },{
+        path : "about",
+        element : <About/>
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
